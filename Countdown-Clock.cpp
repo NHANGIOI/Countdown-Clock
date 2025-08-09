f0121a5bf7407a36ce1f82c0974a11dcc658a9c6
NHANGIOI <nhannguyen120216@gmail.com>
2025-08-10 01:17:56 +0700

    thêm font chữ consolas(regular) và thêm chữ 30/60/auto fps phần setting

--- Countdown-Clock.cpp
+++ Countdown-Clock.cpp
@@ -18,18 +18,27 @@ void draw_reg(sf::Vector2f pos,sf::Vector2f sz,sf::Color color_code,sf::RenderWi
     x.draw(reg);
 }
 void draw_line(sf::Vector2f A,sf::Vector2f B,float do_day,sf::Color color_code,sf::RenderWindow &x){
-    sf::Vector2f n = sf::Vector2f(A.y - B.y,B.x - A.x);
-    float dv = (float)std::sqrt(n.x * n.x + n.y * n.y);
-    n = (n * do_day) / (2 * dv);
+    sf::Vector2f vn = sf::Vector2f(A.y - B.y,B.x - A.x);
+    float dv = (float)std::sqrt(vn.x * vn.x + vn.y * vn.y);
+    vn = (vn * do_day) / (2 * dv);
 
     sf::Vertex diem [4] = {
-        sf::Vertex(sf::Vector2f(A + n)),
-        sf::Vertex(sf::Vector2f(B + n)),
-        sf::Vertex(sf::Vector2f(B - n)),
-        sf::Vertex(sf::Vector2f(A - n))
+        sf::Vertex(sf::Vector2f(A + vn)),
+        sf::Vertex(sf::Vector2f(B + vn)),
+        sf::Vertex(sf::Vector2f(B - vn)),
+        sf::Vertex(sf::Vector2f(A - vn))
     };
     for(int i = 0;i <= 3;++i)   diem[i].color = color_code;
     x.draw(diem,4,sf::Quads);
+}
+void draw_text(std::string vanban,sf::Font &FontU,int sz,sf::Vector2f pos,sf::Color color_code,sf::RenderWindow &windows){
+    sf::Text tx;
+    tx.setString(vanban);
+    tx.setFont(FontU);
+    tx.setCharacterSize(sz);
+    tx.setPosition(pos);
+    tx.setFillColor(color_code);
+    windows.draw(tx);
 }
 namespace setting{
 void logo(sf::RenderWindow &windows,sf::Event &event){
@@ -44,22 +53,50 @@ void logo(sf::RenderWindow &windows,sf::Event &event){
     windows.draw(sp);
 }
 void tab(sf::RenderWindow &windows,sf::Event &event){
-    sf::RenderWindow tab_setting(sf::VideoMode(200,150),"Setting");
-    if(Frame_limit == 0)    tab_setting.setVerticalSyncEnabled(true);
-    else    tab_setting.setFramerateLimit(Frame_limit);
+    sf::RenderWindow tab_setting(sf::VideoMode(350,200),"Setting");
 
+    sf::Font Consolas;//thêm font chữ consolas(regular)
+    if(Consolas.loadFromFile("Consolas-Regular.ttf") == false){
+        exit(-1);
+    }
+    
     while(tab_setting.isOpen()){
+        if(Frame_limit == 0)    tab_setting.setVerticalSyncEnabled(true);
+        else    tab_setting.setFramerateLimit(Frame_limit);
+
         sf::Event event_in_tab;
-        if(tab_setting.pollEvent(event_in_tab) && event_in_tab.type == sf::Event::Closed) tab_setting.close();
         if(windows.pollEvent(event) && event.type == sf::Event::Closed){
             tab_setting.close();
             windows.close();
         }
+        while(tab_setting.pollEvent(event_in_tab)){
+            if(event_in_tab.type == sf::Event::Closed) tab_setting.close();
+            if(event_in_tab.type == sf::Event::MouseButtonPressed){
+                float x = sf::Mouse::getPosition(tab_setting).x;
+                float y = sf::Mouse::getPosition(tab_setting).y;
+                std::cout << x << " " << y << std::endl;
+
+                if(25.f <= x && x <= 125.f && 11.f <= y && y <= 63.f){ //(25,125) / (11,63) (Button fps 35)
+                    Frame_limit = 35;
+                }
+                if(25.f <= x && x <= 125.f && 73.f <= y && y <= 125.f){ //(25,125) / (73,125)
+                    Frame_limit = 60;
+                }
+                if(25.f <= x && x <= 125.f && 135.f <= y && y <= 187.f){ // (25,125) / (135,187)
+                    Frame_limit = 0;
+                }
+            }
+        }
 
         tab_setting.clear(sf::Color::White);
 
-        
+        draw_reg(sf::Vector2f(25,11),sf::Vector2f(125,52),(Frame_limit == 35 ? sf::Color(0,128,0) : sf::Color::Black),tab_setting);
+        draw_reg(sf::Vector2f(25,73),sf::Vector2f(125,52),(Frame_limit == 60 ? sf::Color(0,128,0) : sf::Color::Black),tab_setting);
+        draw_reg(sf::Vector2f(25,135),sf::Vector2f(125,52),(Frame_limit == 0 ? sf::Color(0,128,0) : sf::Color::Black),tab_setting);
 
+        draw_text("35",Consolas,50,sf::Vector2f(60.f,3.f),sf::Color::White,tab_setting);
+        draw_text("60",Consolas,50,sf::Vector2f(60.f,65.f),sf::Color::White,tab_setting);
+        draw_text("auto",Consolas,50,sf::Vector2f(32.5f,125.f),sf::Color::White,tab_setting);
         tab_setting.display();
     }
 }
@@ -71,12 +108,13 @@ signed main()
     m = sf::VideoMode::getDesktopMode().height / 2;
     sf::RenderWindow windows(sf::VideoMode(n,m),"MyTab");
     
-    if(Frame_limit == 0)    windows.setVerticalSyncEnabled(true); // Đồng bộ với tần số làm mới màn hình (VSync)
-    else    windows.setFramerateLimit(Frame_limit);
 
     std::cout << n << " " << m << std::endl;
     
     while(windows.isOpen()){
+        if(Frame_limit == 0)    windows.setVerticalSyncEnabled(true);
+        else    windows.setFramerateLimit(Frame_limit);
+
         sf::Event event;
         while(windows.pollEvent(event)){
             if(event.type == sf::Event::Closed) windows.close();
